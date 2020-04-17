fe1d00090944bb668dbde8fac541a2a8e22b0ac3
Griffin Banta <gbanta@uoregon.edu>
2020-04-17 10:18:51 -0700

    Finished Coding Practice

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,6 +1,41 @@
-// jshint esversion: 6
+// jshint esversion: 7
 
-let containsDuplicates = s => {
+//req 3a
+let revWords1 = (s) => {
+  let splitString = s.split(" ");
+  let finalString = [];
+  for (var i = 0; i < splitString.length; i++) {
+    let word = splitString[i].split("");
+    finalString.push(word.reverse().join(""));
+  }
+  return finalString.join(" ");
+};
+
+//req 3b
+let revWords2 = (s) => {
+  let splitString = s.split(" ");
+  let finalString = [];
+  let reverse = (word) => {
+    let revWord = word.split("").reverse().join("");
+    finalString.push(revWord);
+  };
+  splitString.forEach(reverse);
+  return finalString.join(" ");
+};
+
+//req 3c
+let revWords3 = (s) => {
+  let splitString = s.split(" ");
+  let finalString = [];
+  for (let word of splitString) {
+    let revWord = word.split("").reverse("").join("");
+    finalString.push(revWord);
+  }
+  return finalString.join(" ");
+};
+
+//extra credit
+let containsDuplicates = (s) => {
   let cArr = [0, 0, 0, 0],
     code;
   for (let i in cArr) {
